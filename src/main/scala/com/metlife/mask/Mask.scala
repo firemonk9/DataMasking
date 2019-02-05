a733f0a55b00c710e3afeab7bfa2c8ccabd1dda6
dhiraj <dhiraj.peechara@icloud.com>
2019-02-05 10:31:09 -0500

    wip

--- src/main/scala/com/metlife/mask/Mask.scala
+++ src/main/scala/com/metlife/mask/Mask.scala
@@ -3,51 +3,111 @@ package com.metlife.mask
 //case class MaskType(maskNumber:Option[String],)
 // mask_db
 //
+import javax.crypto.Cipher
+import javax.crypto.spec.SecretKeySpec
+import org.apache.commons.codec.binary.Base64
+import org.apache.hadoop.hive.ql.exec.UDF
 import org.apache.spark.sql.DataFrame
+import javax.crypto.Cipher
+import javax.crypto.spec.SecretKeySpec
+import org.apache.commons.codec.binary.Base64
+import org.apache.hadoop.hive.ql.exec.UDF
+
 
 object Mask {
 
   val maskDatabase = "MASK_DB"
   val REAL_NUMBER = "REAL_NUMBER"
+  val maskKeyColumn = "mask_key_secret"
+  val maskValueColumn = "mask_value"
+
   def main(args: Array[String]): Unit = {
     //create spark context
     //read properties
   }
 
-  def maskColumn(df:DataFrame, columnName:String,globalName:String ,maskType:String, createIfNotExists:Boolean=false,numberOfDigits:Int= -1):DataFrame={
-    if(maskType == REAL_NUMBER)
-      maskNumber(df,columnName,globalName,createIfNotExists,numberOfDigits)
+  def maskColumn(df: DataFrame, columnName: String, globalName: String, maskType: String, createIfNotExists: Boolean = false, numberOfDigits: Int = -1): DataFrame = {
+    if (maskType == REAL_NUMBER)
+      maskNumber(df, columnName, globalName, createIfNotExists, numberOfDigits)
     else
       df
   }
 
-  def maskNumber(df1:DataFrame, columnName:String,globalName:String, createIfNotExists:Boolean=false,numberOfDigits:Int):DataFrame={
+  def maskNumber(dfToMask: DataFrame, columnName: String, globalName: String, createIfNotExists: Boolean = false, numberOfDigits: Int): DataFrame = {
     import org.apache.spark.sql.functions.{min, max}
     import org.apache.spark.sql.Row
 
-    val df = df1.select(columnName)
-//    df.sparkSession.sql("use "+maskDatabase)
+    //    val dfToMask = dfToMask1.select(columnName)
 
-    val  (begin:Int, toAppend:Option[DataFrame], newDF:DataFrame) = if(!df.sparkSession.catalog.tableExists(globalName) && createIfNotExists==false){
-      throw new Exception("Check the name "+globalName+" or set createIfNotExists to true if new table should be created.")
+
+    val (begin: Int, toAppend: Option[DataFrame], newDF: DataFrame) = if (!dfToMask.sparkSession.catalog.tableExists(globalName) && createIfNotExists == false) {
+      throw new Exception("Check the name " + globalName + " or set createIfNotExists to true if new table should be created.")
     }
-    else if(!df.sparkSession.catalog.tableExists(globalName) && createIfNotExists){
-      val begin = if(numberOfDigits == 8) 10000000
-      else if(numberOfDigits == 9) 100000000
-      else if(numberOfDigits == 10) 1000000000
-      else 1000 //TODO change
-      (begin, None, df)
+    else if (!dfToMask.sparkSession.catalog.tableExists(globalName) && createIfNotExists) {
+      val begin = ("1" + "0" * numberOfDigits).toInt
+      (begin, None, dfToMask)
     }
-    else if(df.sparkSession.catalog.tableExists(globalName)){
-      val Row(maxValue: Double) = df.agg(max(df("value"))).head
+    else if (dfToMask.sparkSession.catalog.tableExists(globalName)) {
+      val Row(maxValue: Double) = dfToMask.agg(max(dfToMask(maskValueColumn))).head
       val begin = (maxValue + 1).toInt
-      val existingDF = df.sparkSession.sql("select key,value from "+globalName)
-      val jdf = df.join(existingDF,df(columnName)===existingDF("key")).drop("key").withColumnRenamed("value",columnName)
-      val toAddDf = df.except(jdf.select(columnName))
+
+      val lookupDF = dfToMask.sparkSession.sql("select " + maskKeyColumn + "," + maskKeyColumn + " from " + globalName)
+      val dfToMaskEnc = getEncryptedColumn(dfToMask,columnName)
+
+      val jdf = dfToMaskEnc.join(lookupDF, dfToMaskEnc(columnName) === lookupDF(maskKeyColumn)).drop(maskKeyColumn).withColumnRenamed(maskValueColumn, columnName)
+      val toAddDf = dfToMaskEnc.except(jdf.select(columnName))
       (begin, Some(jdf), toAddDf)
     }
 
+    if (toAppend.isDefined) {
 
-    df
+    }
+
+    dfToMask
   }
+
+
+  /**
+    * This function encrypts the column and makes SHA-2 of the encrypted column.
+    *
+    * @param df
+    * @param colName
+    * @return
+    */
+  def getEncryptedColumn(df: DataFrame, colName: String): DataFrame = {
+    val spark = df.sparkSession
+    import org.apache.spark.sql.functions._
+    spark.udf.register("encryptInt", encryptInt _)
+    spark.udf.register("encryptStr", encryptStr _)
+
+    val encInt = udf("encryptInt")
+
+    val edf = df.withColumn(colName, encInt(df(colName)))
+    edf.withColumn(colName, sha2(edf(colName), 512))
+  }
+
+
+  def encryptInt(col: Int): String = {
+    val password = "abcdefghijklmno1".getBytes()
+    val cipher = Cipher.getInstance("AES/ECB/PKCS5Padding")
+    val keySpec = new SecretKeySpec(password, "AES")
+    cipher.init(Cipher.ENCRYPT_MODE, keySpec)
+    val output = Base64.encodeBase64String(cipher.doFinal(col.toString.getBytes()))
+    output
+  }
+
+  def encryptStr(col: String): String = {
+    val password = "abcdefghijklmno1".getBytes()
+    val cipher = Cipher.getInstance("AES/ECB/PKCS5Padding")
+    val keySpec = new SecretKeySpec(password, "AES")
+    cipher.init(Cipher.ENCRYPT_MODE, keySpec)
+    val output = Base64.encodeBase64String(cipher.doFinal(col.getBytes()))
+    output
+  }
+
+
+  //  spark.udf.register("my_encrypt",encrypt _)
+  //  spark.udf.register("my_decrypt",decrypt _)
+
+
 }
